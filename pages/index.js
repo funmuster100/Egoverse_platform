29f75c9121655a3dc1f5ae86e7de2fdecc0bf1a5
funmuster100 <erneuerung.rondos0z@icloud.com>
2025-06-13 15:51:17 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 50, background: '#0d0d0d', color: '#eee', height: '100vh' }}>
-      <h1>Willkommen bei EgoVerse</h1>
-      <p>Dein digitaler Klon wartet.</p>
-      <a href="/chat" style={{ color: '#00ff88' }}>Zum Chat</a>
-    </div>
-  );
-}
