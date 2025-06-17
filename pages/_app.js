cda27ebdf3f7d9263d7405db6b34fdd20b171ccd
funmuster100 <erneuerung.rondos0z@icloud.com>
2025-06-17 08:53:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/chat.css"; // ← globales CSS laden
-import "../styles/Chat.module.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
