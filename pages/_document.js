5336a01818de9de6c59d1519021773586f27a726
funmuster100 <erneuerung.rondos0z@icloud.com>
2025-06-17 09:08:26 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="de">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
