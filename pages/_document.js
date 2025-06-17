07575b2c2aaf39069258001719ea9b8fcd6cfdd2
funmuster100 <erneuerung.rondos0z@icloud.com>
2025-06-17 09:19:36 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,26 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="de">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="de" data-theme="light">
+        <Head>
+          {/* Google Font – bereits in deiner CSS verwendet */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+          {/* Meta-Optimierungen */}
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#4a90e2" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
